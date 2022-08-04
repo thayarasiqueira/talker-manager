cb13d5ff5eb145090f6ead1925a25a9e2d6b8e3f
Thayara Siqueira <thay.siqu@gmail.com>
2022-08-04 18:28:09 -0300

    feat: creating endpoint GET /talker (req1)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs').promises;
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -10,6 +11,13 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const result = await fs.readFile('./talker.json', 'utf-8');
+  const talkers = JSON.parse(result);
+
+  return res.status(200).json(talkers);
 });
 
 app.listen(PORT, () => {
